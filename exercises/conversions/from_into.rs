9fe6d7753d924933400209d671b6605a8b640b3b
chenxb02@mingyuanyun.com <chenxb02@mingyuanyun.com>
2024-10-18 01:10:48 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,19 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let split: Vec<&str> = s.split(',').collect();
+        let [name, age] = split[..] else { return Person::default() };
+        if name.is_empty() {
+            Person::default()
+        } else {
+            let age = split[1].parse::<usize>();
+            match age {
+                Ok(age) => Person { name: name.to_string(), age },
+                Err(_) => Person::default()
+            }
+        }
     }
 }
 
--- exercises/smart_pointers/box1.rs
+++ exercises/smart_pointers/box1.rs
@@ -18,11 +18,9 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -34,12 +32,15 @@ fn main() {
     );
 }
 
+use crate::List::{Cons, Nil};
+
 pub fn create_empty_list() -> List {
-    todo!()
+    List::Nil
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    let list = Cons(1, Box::new(List::Nil));
+    list
 }
 
 #[cfg(test)]
